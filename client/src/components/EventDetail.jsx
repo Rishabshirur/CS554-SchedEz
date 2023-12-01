d5171687e765eabf52a079f3a94c95601eb65d61
Sachin Devangan <dsachin.98@gmail.com>
2023-12-01 12:26:57 -0500

    formatting of date

--- client/src/components/EventDetail.jsx
+++ client/src/components/EventDetail.jsx
@@ -9,11 +9,16 @@ const [eventDetail, setEventDetail] = useState([]);
 const {eventId} = useParams();
   let auth = getAuth();
 
+  const formatDate = (dateTimeString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    const date = new Date(dateTimeString);
+    return date.toLocaleDateString('en-US', options);
+  };
+
   useEffect(() => {
     const fetchEvents = async () => {
       try {
         const currentUserInfo = await axios.get(`http://localhost:3000/event/detail/${eventId}`);
-        console.log(currentUserInfo)
         setEventDetail(currentUserInfo.data.events[0]);
       } catch (error) {
         console.error('Error fetching events:', error);
@@ -27,10 +32,10 @@ const {eventId} = useParams();
     <div>
       <h2>{eventDetail.event_name}</h2>
       <p>
-        <strong>Start Date and Time:</strong> {eventDetail.start_datetime}
+        <strong>Start Date and Time:</strong> {formatDate(eventDetail.start_datetime)}
       </p>
       <p>
-        <strong>End Date and Time:</strong> {eventDetail.end_datetime}
+        <strong>End Date and Time:</strong> {formatDate(eventDetail.end_datetime)}
       </p>
       <p>
         <strong>Color Code:</strong> {eventDetail.color_code}
