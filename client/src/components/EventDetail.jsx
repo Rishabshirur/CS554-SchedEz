0a646fa9a190245c7b6b351e5ac23ce8c7e60d37
Sachin Devangan <dsachin.98@gmail.com>
2023-12-06 16:52:32 -0500

    delete button

--- client/src/components/EventDetail.jsx
+++ client/src/components/EventDetail.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { getAuth } from 'firebase/auth';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const EventDetail = ({ event }) => {
 const [eventDetail, setEventDetail] = useState([]);
@@ -9,10 +10,21 @@ const [eventDetail, setEventDetail] = useState([]);
 const {eventId} = useParams();
   let auth = getAuth();
 
+  const navigate = useNavigate();
+
   const formatDate = (dateTimeString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
     const date = new Date(dateTimeString);
     return date.toLocaleDateString('en-US', options);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:3000/event/${eventId}`);
+      navigate('/all-events');
+    } catch (error) {
+      console.error('Error deleting event:', error);
+    }
   };
 
   useEffect(() => {
@@ -43,6 +55,7 @@ const {eventId} = useParams();
       <p>
         <strong>Classification:</strong> {eventDetail.classification}
       </p>
+      <button onClick={handleDelete}>Delete Event</button>
     </div>
   );
 };
