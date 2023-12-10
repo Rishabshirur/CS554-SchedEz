750c17ad80fc2ebed574802d26f2e358b8da3344
Sachin Devangan <dsachin.98@gmail.com>
2023-12-10 16:07:32 -0500

    edit event complete

--- client/src/components/EventDetail.jsx
+++ client/src/components/EventDetail.jsx
@@ -4,12 +4,17 @@ import { getAuth } from 'firebase/auth';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
-const EventDetail = ({ event }) => {
-const [eventDetail, setEventDetail] = useState([]);
+const EventDetail = () => {
+  const [eventDetail, setEventDetail] = useState({});
+  const [editedEvent, setEditedEvent] = useState({});
+  const [initialEventDetail, setInitialEventDetail] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-const {eventId} = useParams();
+
+  const { eventId } = useParams();
   let auth = getAuth();
-
   const navigate = useNavigate();
 
   const formatDate = (dateTimeString) => {
@@ -17,6 +22,35 @@ const {eventId} = useParams();
     const date = new Date(dateTimeString);
     return date.toLocaleDateString('en-US', options);
   };
+
+  const formatDateForInput = (dateTimeString) => {
+    const date = new Date(dateTimeString);
+    const pad = (num) => (num < 10 ? `0${num}` : num);
+  
+    const year = date.getFullYear();
+    const month = pad(date.getMonth() + 1);
+    const day = pad(date.getDate());
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+  
+    return `${year}-${month}-${day}T${hours}:${minutes}`;
+  };
+  
+
+  const fetchEventDetails = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/event/detail/${eventId}`);
+      const event = response.data.events[0];
+      setEventDetail(event);
+      setInitialEventDetail(event);
+    } catch (error) {
+      console.error('Error fetching event details:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEventDetails();
+  }, [eventId]);
 
   const handleDelete = async () => {
     try {
@@ -27,37 +61,124 @@ const {eventId} = useParams();
     }
   };
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const currentUserInfo = await axios.get(`http://localhost:3000/event/detail/${eventId}`);
-        setEventDetail(currentUserInfo.data.events[0]);
-      } catch (error) {
-        console.error('Error fetching events:', error);
-      }
-    };
+  const handleEdit = async (e) => {
+    e.preventDefault();
 
-    fetchEvents();
-  }, []);
+     if (new Date(editedEvent.end_datetime) < new Date(editedEvent.start_datetime)) {
+    setErrorMessage('End date cannot be before start date');
+    return; 
+  }
+    try {
+      await axios.patch(`http://localhost:3000/event/${eventId}`, editedEvent);
+      fetchEventDetails();
+      setIsEditing(false);
+      setShowModal(false);
+      setErrorMessage('');
+    } catch (error) {
+      console.error('Error editing event:', error);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setEventDetail(initialEventDetail);
+    setIsEditing(false);
+    setErrorMessage('');
+  };
+
+  const handleEditClick = () => {
+    const { start_datetime, end_datetime, ...otherDetails } = eventDetail;
+    const formattedStartDate = start_datetime ? formatDateForInput(start_datetime) : '';
+    const formattedEndDate = end_datetime ? formatDateForInput(end_datetime) : '';
+  
+    setEditedEvent({
+      ...otherDetails,
+      start_datetime: formattedStartDate,
+      end_datetime: formattedEndDate,
+    });
+  
+    setIsEditing(true);
+    setShowModal(true);
+  };
+  
+  
 
   return (
     <div>
-      <h2>{eventDetail.event_name}</h2>
-      <p>
-        <strong>Start Date and Time:</strong> {formatDate(eventDetail.start_datetime)}
-      </p>
-      <p>
-        <strong>End Date and Time:</strong> {formatDate(eventDetail.end_datetime)}
-      </p>
-      <p>
-        <strong>Color Code:</strong> {eventDetail.color_code}
-      </p>
-      <p>
-        <strong>Classification:</strong> {eventDetail.classification}
-      </p>
-      <button onClick={handleDelete}>Delete Event</button>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      {!isEditing ? (
+        <div>
+          <h2>{eventDetail.event_name}</h2>
+          <p>
+            <strong>Start Date and Time:</strong> {formatDate(eventDetail.start_datetime)}
+          </p>
+          <p>
+            <strong>End Date and Time:</strong> {formatDate(eventDetail.end_datetime)}
+          </p>
+          <p>
+            <strong>Color Code:</strong> {eventDetail.color_code}
+          </p>
+          <p>
+            <strong>Classification:</strong> {eventDetail.classification}
+          </p>
+          
+          <button onClick={handleEditClick}>Edit Event</button>
+          <button onClick={handleDelete}>Delete Event</button>
+        </div>
+      ) : (
+        showModal && (
+          <div className={`modal ${showModal ? 'active' : ''}`}>
+            <button onClick={handleCloseModal}>Close Modal</button>
+            <h2>Edit Event</h2>
+            <form onSubmit={handleEdit}>
+              <label>
+                Event Name:
+                <input
+                  type="text"
+                  value={editedEvent.event_name || ''}
+                  onChange={(e) => setEditedEvent({ ...editedEvent, event_name: e.target.value })}
+                />
+              </label>
+              <label>
+                Start Date and Time:
+                <input
+                  type="datetime-local"
+                  value={editedEvent.start_datetime || ''}
+                  onChange={(e) => setEditedEvent({ ...editedEvent, start_datetime: e.target.value })}
+                />
+              </label>
+              <label>
+                End Date and Time:
+                <input
+                  type="datetime-local"
+                  value={editedEvent.end_datetime || ''}
+                  onChange={(e) => setEditedEvent({ ...editedEvent, end_datetime: e.target.value })}
+                />
+              </label>
+              <label>
+                Color Code:
+                <input
+                  type="text"
+                  value={editedEvent.color_code || ''}
+                  onChange={(e) => setEditedEvent({ ...editedEvent, color_code: e.target.value })}
+                />
+              </label>
+              <label>
+                Classification:
+                <input
+                  type="text"
+                  value={editedEvent.classification || ''}
+                  onChange={(e) => setEditedEvent({ ...editedEvent, classification: e.target.value })}
+                />
+              </label>
+              <button type="submit">Save Changes</button>
+            </form>
+          </div>
+        )
+      )}
     </div>
   );
 };
 
 export default EventDetail;
+
